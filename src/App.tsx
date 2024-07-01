4ff22f6c7f271802805a3a6c5a96a8b09b4c862c
Janko Ljubic <ljubicjanko1@gmail.com>
2024-07-01 22:47:05 +0200

    Change to node

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header.component';
+import routes from './routes';
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <div className="main-container">
       <Header />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import routes from './routes';
-import Header from './components/header/Header.component';
 
-const router = createBrowserRouter(routes);
-
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="main-container">
-      <Header />
-      <RouterProvider router={router} />
-    </div>
+    <App />
   </React.StrictMode>
 );
