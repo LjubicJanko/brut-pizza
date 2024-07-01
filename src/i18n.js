95360a8ba22b493baa35261e7a893cf7b2a161f2
Janko Ljubic <ljubicjanko1@gmail.com>
2024-07-01 23:10:46 +0200

    setup translation

---
+++ src/i18n.js
@@ -0,0 +1,38 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    backend: {
+      loadPath: 'public/locales/{{lng}}/{{ns}}.json',
+    },
+    detection: {
+      order: [
+        'querystring',
+        'localStorage',
+        'cookie',
+        'navigator',
+        'htmlTag',
+        'path',
+        'subdomain',
+      ],
+      lookupQuerystring: 'language',
+    },
+    returnEmptyString: false,
+    ns: 'translation',
+    defaultNS: 'translation',
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
 import { PATH } from '../../routes';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+  const { t } = useTranslation();
   return (
     <>
+      {t('key')}
       <ul>
         <li>
           <Link to={PATH.MENU}>Menu</Link>
