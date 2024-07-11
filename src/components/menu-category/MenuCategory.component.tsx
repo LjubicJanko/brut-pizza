0c2edf1bea005975eb0b2fd70986c00cb6dd9a6c
Janko Ljubic <ljubicjanko1@gmail.com>
2024-07-11 12:38:00 +0200

    scroll animation for menu items

--- src/components/menu-category/MenuCategory.component.tsx
+++ src/components/menu-category/MenuCategory.component.tsx
@@ -1,4 +1,5 @@
 import MenuItem, { MenuItemType } from '../menu-item/MenuItem.component';
+import ScrollItem from '../scroll-item/ScrollItem.component';
 import * as Styled from './MenuCategory.styles';
 
 export type MenuCategoryProps = {
@@ -20,20 +21,24 @@ export const MenuCategory = ({
     <Styled.MenuCategoryContainer id={sectionId} className={className}>
       <div className="section-items">
         {items?.map((item, index) => (
-          <MenuItem key={index} {...item} />
+          <ScrollItem key={index}>
+            <MenuItem {...item} />
+          </ScrollItem>
         ))}
       </div>
       {!!additions && (
         <>
           <h2 className="section-subtitle">{subtitle}</h2>
           <div className="section-additions">
-            {additions?.map((addition, index) => (
-              <MenuItem
-                key={index}
-                className="section-additions-item"
-                {...addition}
-              />
-            ))}
+            <ScrollItem>
+              {additions?.map((addition, index) => (
+                <MenuItem
+                  key={index}
+                  className="section-additions-item"
+                  {...addition}
+                />
+              ))}
+            </ScrollItem>
           </div>
         </>
       )}
