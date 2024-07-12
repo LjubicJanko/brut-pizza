a48b24e95ec6ef94ded9b0ac00fe629b43aeb3d2
Janko Ljubic <ljubicjanko1@gmail.com>
2024-07-12 15:28:09 +0200

    change place of the font

--- src/styles/FontStyles.tsx
+++ src/styles/FontStyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const FontStyles = createGlobalStyle`
     @font-face {
         font-family: 'Andika';
-        src: url('../../../public/fonts/Andika-Regular.ttf') format('truetype');
+        src: url('../../fonts/Andika-Regular.ttf') format('truetype');
         font-display: swap;
     };
 `;
